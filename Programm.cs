7516304c7614e7e961a9eecdede400f6ddfce2f0
MolodoyVan <92540233+MolodoyVan@users.noreply.github.com>
2022-02-04 17:06:50 +0300

    Update Programm.cs

--- Programm.cs
+++ Programm.cs
@@ -9,19 +9,24 @@ namespace Мини_игра
             Dungeon1 dun1 = new Dungeon1();
             Dungeon2 dun2 = new Dungeon2();
             Walking wal = new Walking();
+            Fighting fight = new Fighting();
 
             int x = 12;
             bool IsTrue = true;
             string[,] w = new string[x, x];
+
             w = dun1.Peredacha();
             wal.Vvod = w;
             wal.Pohod();
+
             w = dun2.Peredacha();
             wal.Vvod = w;
             wal.Proverka = IsTrue;
             wal.Pohod();
 
+            fight.Fight();
             //Console.WriteLine("Hello, Server!");
         }
     }
 }
+//Проект закончен. Кринж полный, но для певрого раза думаю неплохо
